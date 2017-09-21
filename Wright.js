219a90ac9e373a740dec2f7076dd5fb78552d25f
Nile Walker <niwal7@morgan.edu>
2017-09-21 11:10:30 -0400

    CheckLegal
    Added a function to check if a pose is held for 3/4s of a second before executing+Moved the poses into an array

--- Wright.js
+++ Wright.js
@@ -25,34 +25,56 @@ process.stdin.on('keypress', function (ch, key) {
    console.log("disable emergency");
   }
 });
-
+takecomm=true
 var flying=false;
 // speeds can be anything between 0 and 1
 var yawSpeed = 0.5;
 var pitchSpeed = 0.5;
 var rollSpeed = 0.5;
-
+poses=['fist','fingers_spread','double_tap','wave_in','wave_out']
 myMyo.on('connect', function() {
   console.log("I'm Alive");
 });
 console.log("begin");
-  
+myMyo.setLockingPolicy('none')  
+myMyo.on('lock', function(){ myMyo.unlock('hold') })
+
+//Lands if Myo is removed or unresponsive
+myMyo.on('arm_unsynced', function(){
+  console.log("Houston we have a problem")
+  land
+  })
+
 myMyo.on('pose', function(pose) {
+  //Checks that a pose is held for 3/4s of a second before executing
+  function CheckLegal(time){
+    hut=true
+    while (hut){
+  myMyo.on('pose_off',function(){
+    //myMyo.trigger('pose','rest')
+    return false;
+  })
+  return true;
+  setTimeout(function(){hut=false},750)
+}
+}
+  takecomm=CheckLegal()
   // takeoff
-  if (pose == 'fist')
+  if (takecomm){
+  if (pose == poses[0])
   {
     console.log("landing")
     client.land()
   }
   // landing
-  if ((pose == 'fingers_spread'))
+  if ((pose == poses[1]))
   {
     client.disableEmergency(); // in case drone crashed on previous run
     flying = true; 
     console.log("taking flight");
     client.takeoff();
     }
-  else if ((pose == 'double_tap')){
+  else if ((pose == poses[2])){
 
     flying = true;
     console.log("Do A Barrel Roll");
@@ -60,17 +82,17 @@ myMyo.on('pose', function(pose) {
     
     
   }
-  else if ((pose == 'wave_in')){
+  else if ((pose == poses[3])){
 
     console.log("left");
     client.left(rollSpeed);
     
     
   }
-  else if ((pose == 'wave_out')){
+  else if ((pose == poses[4])){
 
     console.log("right");
     client.right(rollSpeed); 
   }
-  });
+  }});
 
